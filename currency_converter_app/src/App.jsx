e809864a1925ba2dacee2900f8cfc0ee19cf4d88
Emma_Tetteh <tettehe7@gmail.com>
2024-09-25 11:12:53 +0200

    new commit

---
+++ currency_converter_app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-gray-300 font-bold'>My name is Tetteh
+    </div>
+  )
+}
+
+export default App
