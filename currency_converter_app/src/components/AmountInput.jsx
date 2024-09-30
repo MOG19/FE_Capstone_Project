382b410db1e2f065ce53cd7074d26f136975b8e3
Emma_Tetteh <tettehe7@gmail.com>
2024-09-30 12:27:53 +0200

    update App.jsx

---
+++ currency_converter_app/src/components/AmountInput.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AmountInput = () => {
+  return (
+    <div className='bg-pink-200'>
+        Nane
+    </div>
+  )
+}
+
+export default AmountInput;
---
+++ currency_converter_app/src/components/CurrencySelector.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CurrencySelector = () => {
+  return (
+    <div><input type="number"/>
+    <select name="" id="">
+        <option value=""><h1>Hi</h1></option>
+    </select>
+    </div>
+  )
+}
+
+export default CurrencySelector;
